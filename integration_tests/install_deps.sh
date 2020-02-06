445221bb8e531d77aad987b936b30d385521134f
Tommy Carpenter <tc677g@att.com>
2020-02-06 10:53:22 -0500

    Update dep script; no version/code changes
    (a1 does not even currently use this script, yet. this is mostly
     documentation right now)
    
    Change-Id: Ibe4daef77398bd0ebd3dbf3a44849026df931f05
    Signed-off-by: Tommy Carpenter <tc677g@att.com>

--- integration_tests/install_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git clone --branch 1.10.2 https://gerrit.oran-osc.org/r/ric-plt/lib/rmr \
-    && cd rmr \
-    && mkdir .build; cd .build; cmake .. -DPACK_EXTERNALS=1; sudo make install \
-    && cd ../.. \
-    && rm -rf rmr
